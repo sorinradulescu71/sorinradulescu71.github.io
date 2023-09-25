616727c1211db1698ead8e69ce27551d505cabb2
sorinradulescu71 <131678477+sorinradulescu71@users.noreply.github.com>
2023-09-25 13:11:06 +0300

    Add files via upload

--- ChatGptWidget_Builder.js
+++ ChatGptWidget_Builder.js
@@ -93,6 +93,7 @@ input[type="color"]::-webkit-color-swatch {
     </table>
     <input value="Update Settings" type="submit">
     <br>
+    <p>Developed by <a target="_blank" href="https://linkedin.com/in/itsrohitchouhan">Rohit Chouhan</a></p>
 </form>
 `;
    class ChatGptWidgetBuilderPanel extends HTMLElement {
@@ -135,7 +136,7 @@ input[type="color"]::-webkit-color-swatch {
       }
 
    }
-   customElements.define("sorin-chatgptwidget-builder",
+   customElements.define("com-rohitchouhan-sap-chatgptwidget-builder",
       ChatGptWidgetBuilderPanel
    );
 })();
