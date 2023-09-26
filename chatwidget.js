f05c5b6574ea42ddc9f3956a4c49f964172042f1
sorinradulescu71 <131678477+sorinradulescu71@users.noreply.github.com>
2023-09-26 10:37:53 +0300

    Add files via upload

---
+++ chatwidget.js
@@ -0,0 +1,54 @@
+var ajaxCall = (key, url, prompt) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 1024,
+        n: 1,
+        temperature: 0.5,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+      crossDomain: true,
+      success: function (response, status, xhr) {
+        resolve({ response, status, xhr });
+      },
+      error: function (xhr, status, error) {
+        const err = new Error('xhr error');
+        err.status = xhr.status;
+        reject(err);
+      },
+    });
+  });
+};
+
+const url = "https://sce-pal-genai-test.openai.azure.com/v1";
+const apiVersion = "2023-06-01-preview";
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    async post(apiKey, endpoint, prompt) {
+      const { response } = await ajaxCall(
+        apiKey,
+        `${url}/${apiVersion}${endpoint}`,
+        prompt
+      );
+      console.log(response.choices[0].text);
+      return response.choices[0].text;
+    }
+  }
+  customElements.define("custom-widget", MainWebComponent);
+})();
